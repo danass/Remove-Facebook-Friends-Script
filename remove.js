8884cb6c1def4dee50d927ddb9612d23190223e1
Daniel Assayag <11842639+danass@users.noreply.github.com>
2024-06-20 19:17:38 +0200

    Update remove.js
    updating code based on facebook new selectors

--- remove.js
+++ remove.js
@@ -1,54 +1,46 @@
-// go to https://m.facebook.com/friends/center/friends 
-// open console and paste the code
-//then paste the following code
+// Go to https://m.facebook.com/friends/center/friends 
+// Open the console and type "allow pasting" enter, and paste the code enter.
 
-    //retrieving all page html
-    ok = this.document
+function getRandomArbitrary(min, max) { return Math.random() * (max - min) + min; }
+function stopInterval() { clearInterval(interval); }
+function greet() { removeFriend().next(); }
 
-    //finding the remove friend button div, and storing a copy of it
-    firstrec = ok.firstChild.nextSibling.firstChild.nextElementSibling.firstChild.nextElementSibling.firstChild.nextElementSibling.firstChild.nextElementSibling.nextElementSibling.nextElementSibling.firstElementChild.firstElementChild.firstElementChild.nextElementSibling.firstElementChild
-    nextrec = firstrec
-
-    // simulation of a click function for all scenarios
-    async function clickf(div, entry) {
-        if (entry == null) {
-            await (div).click()
-            return await (div).click()
-        }
-        if (entry == "0") {
-            await (div).click()
-            return await (div).click()
-        }
-    }
-
-
-    function* removefb() {
-        while (true) {
-            //might be better to add a key listener to stop whenever you want.
-            nextclick = nextrec.firstElementChild.lastChild.lastElementChild.lastElementChild.lastElementChild.lastChild
-
-            
-            nextreccopy = nextrec
-            nextrec = nextrec.nextSibling
-            if (nextrec == null) {
-                nextrec = nextreccopy
-                nextrec = nextrec.parentElement.nextElementSibling.firstElementChild
-            }
-            clickf(nextclick)
-
-            remover = nextclick.nextElementSibling.firstElementChild.firstElementChild.firstElementChild.nextElementSibling.firstElementChild.nextElementSibling
-            console.log(remover)
-            clickf(remover, 0)
-            yield
-        }
-    }
-
-    function greet() {
-        removefb().next()
-    }
-function getRandomArbitrary(min, max) {
-  return Math.random() * (max - min) + min;
+function startInterval() {
+    // Set interval to a large value to avoid suspicion from Facebook
+    interval = setInterval(greet, getRandomArbitrary(2000, 5500));
+    return interval;
 }
-    // interval set to a large value, so fb doesn't identify you as suspicious
-    setInterval(greet, getRandomArbitrary(2000,5500));
+async function clickElement(element, entry) {
+    if (entry == null || entry == "0") {
+        await element.click();
+        return await element.click();
+    }
+}
+
+let intervalId = startInterval();
+
+function* removeFriend() {
+    while (true) {
+        nextClick = nextRecord.lastElementChild.firstChild.firstChild.lastChild.firstChild;
+        nextRecordCopy = nextRecord;
+        nextRecord = nextRecord.nextSibling;
+
+        if (nextRecord == null) {
+            nextRecord = nextRecordCopy;
+            nextRecord = nextRecord?.parentElement?.nextElementSibling?.firstElementChild;
+
+            if (nextRecord == undefined) {
+                stopInterval(intervalId);
+            }
+        }
         
+        clickElement(nextClick);
+        remover = nextClick.nextElementSibling.firstElementChild.firstElementChild.firstElementChild.nextElementSibling.firstElementChild.nextElementSibling;
+        clickElement(remover, 0);
+        yield;
+    }
+}
+
+currentPage = this.document;
+firstRecord = currentPage.firstChild.nextSibling.firstChild.nextElementSibling.firstChild.nextElementSibling.firstChild.nextElementSibling.firstChild.nextElementSibling.nextElementSibling.nextElementSibling.firstElementChild.firstElementChild.firstElementChild.nextElementSibling.firstElementChild;
+nextRecord = firstRecord;
